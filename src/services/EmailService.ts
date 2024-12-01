b98a96b5436a39ea379eed03a2c0b2b4849eba91
Proff <119138601+proffsmoke@users.noreply.github.com>
2024-12-01 20:49:10 -0300

    0.0.0.0 test

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -12,7 +12,7 @@ class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: '127.0.0.1',
+      host: '0.0.0.0',
       port: 25,
       secure: false,
       auth: {
