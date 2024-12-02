b3205959ef9f92fba3d5ac0d9acea02ea3ffdcf2
Proff <119138601+proffsmoke@users.noreply.github.com>
2024-12-01 21:03:24 -0300

    0.0.0.0 test

--- dist/services/PortCheckService.js
+++ dist/services/PortCheckService.js
@@ -6,30 +6,46 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const net_1 = __importDefault(require("net"));
 const logger_1 = __importDefault(require("../utils/logger"));
 class PortCheckService {
-    async checkPort(ip, port) {
+    /**
+     * Testa uma conexão com um host e porta específicos.
+     * @param host Host para testar (ex.: smtp.gmail.com)
+     * @param port Porta para testar (ex.: 25)
+     * @returns True se a conexão for bem-sucedida, False caso contrário.
+     */
+    async checkPort(host, port) {
         return new Promise((resolve) => {
             const socket = new net_1.default.Socket();
-            socket.setTimeout(5000);
-            socket.on('connect', () => {
+            socket.setTimeout(5000); // Define o timeout de 5 segundos
+            socket
+                .on('connect', () => {
                 socket.destroy();
-                resolve(true);
-            }).on('timeout', () => {
+                resolve(true); // Conexão bem-sucedida
+            })
+                .on('timeout', () => {
                 socket.destroy();
-                resolve(false);
-            }).on('error', () => {
-                resolve(false);
-            }).connect(port, ip);
+                resolve(false); // Timeout
+            })
+                .on('error', () => {
+                resolve(false); // Qualquer outro erro
+            })
+                .connect(port, host); // Conecta ao host e porta fornecidos
         });
     }
-    async verifyPort(ip = '0.0.0.0', ports) {
+    /**
+     * Verifica múltiplas portas em um host.
+     * @param host Host para testar (ex.: smtp.gmail.com)
+     * @param ports Lista de portas para verificar (ex.: [25, 587, 465])
+     * @returns A primeira porta aberta encontrada ou null se nenhuma estiver aberta.
+     */
+    async verifyPort(host, ports) {
         for (const port of ports) {
-            if (await this.checkPort(ip, port)) {
-                logger_1.default.info(`Verificação de porta ${port} para IP ${ip}: ABERTA`);
-                return port;
+            if (await this.checkPort(host, port)) {
+                logger_1.default.info(`Verificação de porta ${port} para host ${host}: ABERTA`);
+                return port; // Retorna a primeira porta aberta
             }
         }
-        logger_1.default.warn(`Nenhuma porta disponível para IP ${ip}`);
-        return null;
+        logger_1.default.warn(`Nenhuma porta disponível para host ${host}`);
+        return null; // Nenhuma porta encontrada
     }
 }
 exports.default = new PortCheckService();
