82151bb98206f426fae4c6fe1e90dc6bcfa29cf9
Proff <119138601+proffsmoke@users.noreply.github.com>
2025-01-10 12:09:07 -0300

    clean logs auto 30 min

--- src/models/EmailLog.ts
+++ src/models/EmailLog.ts
@@ -8,6 +8,7 @@ export interface IEmailLog extends Document {
   success: boolean | null;
   detail: Record<string, any>; // Removido o '?'
   sentAt: Date;
+  expireAt: Date; // Novo campo para expiração
 }
 
 const EmailLogSchema: Schema = new Schema(
@@ -19,11 +20,15 @@ const EmailLogSchema: Schema = new Schema(
     success: { type: Boolean, default: null },
     detail: { type: Schema.Types.Mixed, default: {} }, // Garante que detail nunca seja undefined
     sentAt: { type: Date, default: Date.now, index: true },
+    expireAt: { type: Date, default: () => new Date(Date.now() + 30 * 60 * 1000), index: true }, // Expira em 30 minutos
   },
   {
     timestamps: true,
     collection: 'emailLogs', // Especifica explicitamente o nome da coleção
   }
 );
+
+// Criar índice TTL no campo expireAt
+EmailLogSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
 export default mongoose.model<IEmailLog>('EmailLog', EmailLogSchema);
