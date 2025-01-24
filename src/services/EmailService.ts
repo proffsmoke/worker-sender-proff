90289106f2add60d6f3eb0fa085f853ed98c5dfd
Proff <119138601+proffsmoke@users.noreply.github.com>
2025-01-24 13:50:00 -0300

    up de milhao

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -56,14 +56,23 @@ class EmailService {
     return EmailService.instance;
   }
 
+  private createRecipientsStatus(recipients: string[], success: boolean, error?: string, queueId?: string): RecipientStatus[] {
+    return recipients.map((recipient) => ({
+      recipient,
+      success,
+      error,
+      queueId,
+    }));
+  }
+
   public async sendEmail(params: SendEmailParams, uuid?: string): Promise<SendEmailResult> {
     const { fromName = 'No-Reply', emailDomain, to, bcc = [], subject, html, clientName } = params;
     const from = `"${fromName}" <${process.env.MAILER_NOREPLY_EMAIL || 'no-reply@outlook.com'}>`;
-  
+
     const toRecipients: string[] = Array.isArray(to) ? to.map((r) => r.toLowerCase()) : [to.toLowerCase()];
     const bccRecipients: string[] = bcc.map((r) => r.toLowerCase());
     const allRecipients: string[] = [...toRecipients, ...bccRecipients];
-  
+
     try {
       const mailOptions = {
         from,
@@ -72,83 +81,64 @@ class EmailService {
         subject: clientName ? `[${clientName}] ${subject}` : subject,
         html,
       };
-  
+
       const info = await this.transporter.sendMail(mailOptions);
-  
+
       const queueIdMatch = info.response.match(/queued as\s([A-Z0-9]+)/);
       if (!queueIdMatch || !queueIdMatch[1]) {
         throw new Error('Não foi possível extrair o queueId da resposta');
       }
-  
+
       const queueId = queueIdMatch[1];
       logger.info(`extraído com sucesso de response:${info.response}, queueId: ${queueId}`);
       logger.info(`Email enviado!`);
-  
-      // Verifica se o queueId já foi processado
+
       if (this.stateManager.isQueueIdAssociated(queueId)) {
         logger.warn(`queueId ${queueId} já foi processado. Ignorando duplicação.`);
         return {
           queueId,
-          recipients: allRecipients.map((recipient) => ({
-            recipient,
-            success: true,
-            queueId,
-          })),
+          recipients: this.createRecipientsStatus(allRecipients, true, undefined, queueId),
         };
       }
 
-      // Associa o queueId ao UUID
       if (uuid) {
         this.stateManager.addQueueIdToUuid(uuid, queueId);
         logger.info(`Associado queueId ${queueId} ao UUID ${uuid}`);
       }
 
-      const recipientsStatus: RecipientStatus[] = allRecipients.map((recipient) => ({
-        recipient,
-        success: true,
-        queueId,
-      }));
-  
+      const recipientsStatus = this.createRecipientsStatus(allRecipients, true, undefined, queueId);
+
       this.stateManager.addPendingSend(queueId, {
         toRecipients,
         bccRecipients,
         results: recipientsStatus,
       });
-  
+
       return {
         queueId,
         recipients: recipientsStatus,
       };
     } catch (error: any) {
       logger.error(`Erro ao enviar email: ${error.message}`, error);
-  
-      const recipientsStatus: RecipientStatus[] = allRecipients.map((recipient) => ({
-        recipient,
-        success: false,
-        error: error.message,
-      }));
-  
+
       return {
         queueId: '',
-        recipients: recipientsStatus,
+        recipients: this.createRecipientsStatus(allRecipients, false, error.message),
       };
     }
   }
 
   private handleLogEntry(logEntry: LogEntry): void {
     const sendData = this.stateManager.getPendingSend(logEntry.queueId);
-    if (!sendData) {
-      //logger.warn(`Nenhum dado encontrado no pendingSends para queueId=${logEntry.queueId}. Log completo: ${JSON.stringify(logEntry)}`);
-      return;
-    }
-    
+    if (!sendData) return;
+
     const success = logEntry.success;
     const recipient = logEntry.email.toLowerCase();
-  
+
     const recipientIndex = sendData.results.findIndex((r) => r.recipient === recipient);
     if (recipientIndex !== -1) {
       sendData.results[recipientIndex].success = success;
-      
+
       if (!success) {
         sendData.results[recipientIndex].error = `Status: ${logEntry.result}`;
         logger.error(`Falha ao enviar para recipient=${recipient}. Erro: ${logEntry.result}. Log completo: ${JSON.stringify(logEntry)}`);
@@ -158,22 +148,22 @@ class EmailService {
     } else {
       logger.warn(`Recipient ${recipient} não encontrado nos resultados para queueId=${logEntry.queueId}. Log completo: ${JSON.stringify(logEntry)}`);
     }
-    
+
     const totalRecipients = sendData.toRecipients.length + sendData.bccRecipients.length;
     const processedRecipients = sendData.results.length;
-    
+
     logger.debug(`Status de processamento para queueId=${logEntry.queueId}: Total de recipients=${totalRecipients}, Processados=${processedRecipients}. Log completo: ${JSON.stringify(logEntry)}`);
-    
+
     if (processedRecipients >= totalRecipients) {
       logger.info(`Todos os recipients processados para queueId=${logEntry.queueId}. Removendo do pendingSends. Status atual: ${JSON.stringify(sendData)}`);
       this.stateManager.deletePendingSend(logEntry.queueId);
-      
+
       const uuid = this.stateManager.getUuidByQueueId(logEntry.queueId);
       if (uuid) {
         logger.info(`Atualizando status para queueId=${logEntry.queueId} com UUID=${uuid}.`);
         this.stateManager.updateQueueIdStatus(logEntry.queueId, success, uuid);
       }
-    
+
       if (uuid && this.stateManager.isUuidProcessed(uuid)) {
         const results = this.stateManager.consolidateResultsByUuid(uuid);
         if (results) {
