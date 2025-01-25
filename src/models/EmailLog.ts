1b51f48b524320525c0174ab125b46baf4ef5051
Proff <119138601+proffsmoke@users.noreply.github.com>
2025-01-24 21:51:45 -0300

    up

--- src/models/EmailLog.ts
+++ src/models/EmailLog.ts
@@ -7,7 +7,6 @@ export interface IEmailLog extends Document {
   success: boolean | null; // Status do envio (true, false, ou null se ainda não processado)
   updated: boolean; // Indica se o log foi atualizado por outro serviço
   sentAt: Date; // Data de envio
-  expireAt: Date; // Data de expiração
 }
 
 const EmailLogSchema: Schema = new Schema(
@@ -18,7 +17,6 @@ const EmailLogSchema: Schema = new Schema(
     success: { type: Boolean, default: null }, // Inicialmente null
     updated: { type: Boolean, default: false }, // Inicialmente false
     sentAt: { type: Date, default: Date.now, index: true },
-    expireAt: { type: Date, default: () => new Date(Date.now() + 30 * 60 * 1000), index: true }, // Expira em 30 minutos
   },
   {
     timestamps: true,
@@ -26,7 +24,6 @@ const EmailLogSchema: Schema = new Schema(
   }
 );
 
-// Criar índice TTL no campo expireAt
-EmailLogSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
+
 
 export default mongoose.model<IEmailLog>('EmailLog', EmailLogSchema);
