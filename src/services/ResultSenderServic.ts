95ec3af327d78de80bdfffe0a5a5dac773876749
Proff <119138601+proffsmoke@users.noreply.github.com>
2025-01-27 01:12:56 -0300

    add result service

--- src/services/ResultSenderServic.ts
+++ src/services/ResultSenderServic.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosError } from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { z } from 'zod'; // Importando Zod para validação
 import EmailQueueModel from '../models/EmailQueueModel';
 import logger from '../utils/logger';
