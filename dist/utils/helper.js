c2504747331b94a39a56e1d95fcc90c2f8870cec
Proff <proff@pop-os.local>
2026-01-08 19:03:47 -0300

    chore: remove legacy dist artifacts

--- dist/utils/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delay = delay;
-// src/utils/helper.ts
-function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
